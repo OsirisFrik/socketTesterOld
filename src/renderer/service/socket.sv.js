991584623e15f0d13f9546fb5b1c2fd48712509d
OsirisFrik <osiris.lpz96@gmail.com>
2018-10-22 16:11:16 -0700

    remove event

--- src/renderer/service/socket.sv.js
+++ src/renderer/service/socket.sv.js
@@ -91,15 +91,6 @@ export default {
       })
     })
 
-    socket.on('disconnect', reason => {
-      console.warn(reason)
-      tab.socket = null
-      toast({
-        message: `Socket ${tab.name} disconnected`,
-        color: 'error'
-      })
-    })
-
     socket.on('connect', () => {
       toast({
         message: `Socket ${tab.name} connected`
